2f3fad8239953b50f687c9d4108bd4c778de4321
NitishMishra0 <nitishmishrani@gmail.com>
2024-06-16 21:52:18 +0530

    created user update api

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { testController } from '../controllers/user.js';
+import { testController, updateUser } from '../controllers/user.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const userRouter=express.Router();
 
 userRouter.get('/test',testController);
+userRouter.post('/update/:id',verifyToken,updateUser);
 
 export default userRouter;
